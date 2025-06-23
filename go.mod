b4e84ad681a5cce69b80dd6d99a10c4f51309fd3
kumarabd <abishekkumar92@gmail.com>
2025-06-22 20:35:06 -0700

    changed logger

--- go.mod
+++ go.mod
@@ -10,13 +10,12 @@ require (
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/prometheus/client_golang v0.9.3
-	github.com/sirupsen/logrus v1.7.0
+	github.com/rs/zerolog v1.34.0
 	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.6.1 // indirect
-	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
--- logger/logger.go
+++ logger/logger.go
@@ -2,97 +2,18 @@ package logger
 
 import (
 	"os"
-	"time"
 
-	"github.com/realnighthawk/bucky/errors"
-	"github.com/sirupsen/logrus"
+	"github.com/rs/zerolog"
 )
 
-// Handler is the interface for bucky logger
-type Handler interface {
-	Info(description ...interface{})
-	Debug(description ...interface{})
-	Warn(err error)
-	Error(err error)
-	WithField(string, interface{}) *Logger
-	WithFields(map[string]interface{}) *Logger
-}
-
-// Logger is the implementation of Handler interface
-type Logger struct {
-	handler *logrus.Entry
-}
+// Handler is a type alias for zerolog.Logger
+type Handler = zerolog.Logger
 
 // New instantiates bucky logger instance
-func New(appname string, opts Options) (Handler, error) {
+func New(appname string, opts Options) (*Handler, error) {
+	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
+	logger = logger.With().Str("app", appname).Logger()
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
 
-	log := logrus.New()
-
-	switch opts.Format {
-	case JSONLogFormat:
-		log.SetFormatter(&logrus.JSONFormatter{
-			TimestampFormat: time.RFC3339,
-		})
-	case SyslogLogFormat:
-		log.SetFormatter(&logrus.TextFormatter{
-			TimestampFormat: time.RFC3339,
-			FullTimestamp:   true,
-		})
-	}
-
-	// log.SetReportCaller(true)
-	log.SetOutput(os.Stdout)
-	entry := log.WithFields(logrus.Fields{
-		"app": appname,
-	})
-
-	return &Logger{
-		handler: entry,
-	}, nil
-}
-
-// Error logs for log-level error
-func (l *Logger) Error(err error) {
-	if errors.Is(err) {
-		l.handler.WithFields(logrus.Fields{
-			"code":     errors.GetCode(err),
-			"severity": errors.GetSeverity(err),
-		}).Log(logrus.ErrorLevel, err.Error())
-	}
-}
-
-// Warn logs for log-level warning
-func (l *Logger) Warn(err error) {
-	if errors.Is(err) {
-		l.handler.WithFields(logrus.Fields{
-			"code":     errors.GetCode(err),
-			"severity": errors.GetSeverity(err),
-		}).Log(logrus.WarnLevel, err.Error())
-	}
-}
-
-// Info logs for log-level info
-func (l *Logger) Info(description ...interface{}) {
-	l.handler.Log(logrus.InfoLevel,
-		description...,
-	)
-}
-
-// Debug logs for log-level debug
-func (l *Logger) Debug(description ...interface{}) {
-	l.handler.Log(logrus.DebugLevel,
-		description...,
-	)
-}
-
-// WithField logs with added field
-func (l *Logger) WithField(s string, val interface{}) *Logger {
-	l.handler = l.handler.WithField(s, val)
-	return l
-}
-
-// WithFields logs with given added fields
-func (l *Logger) WithFields(d map[string]interface{}) *Logger {
-	l.handler = l.handler.WithFields(logrus.Fields(d))
-	return l
+	return &logger, nil
 }
